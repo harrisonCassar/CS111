756f6a2510562a783e94d6216e5fe23ff8628815
harrisonCassar - Desktop <harrison.cassar@gmail.com>
2020-03-10 11:16:23 -0700

    Add support for inode allocation audits (with modifications to data structs holding report information, from stroing in str to int)

--- lab3b/lab3b.py
+++ lab3b/lab3b.py
@@ -1,62 +1,118 @@
+#import libraries
 import csv
 
+#define auxiliary data structures
 class Superblock:
-	def __init__(self):
-		self.blocks_count = -1
-		self.inodes_count = -1
-		self.block_size = -1
-		self.inode_size = -1
-		self.blocks_per_group = -1
-		self.inodes_per_group = -1
-		self.first_non_reserved_inode = -1
+	def __init__(self, report):
+		self.blocks_count = int(report[1])
+		self.inodes_count = int(report[2])
+		self.block_size = int(report[3])
+		self.inode_size = int(report[4])
+		self.blocks_per_group = int(report[5])
+		self.inodes_per_group = int(report[6])
+		self.first_non_reserved_inode = int(report[7])
 
 class Group:
-	def __init__(self):
-		self.group_num = -1
-		self.num_blocks_in_group = -1
-		self.num_inodes_in_group = -1
-		self.num_free_blocks = -1
-		self.num_free_inodes = -1
-		self.block_bitmap = -1
-		self.inode_bitmap = -1
-		self.inode_table = -1
+	def __init__(self, report):
+		self.group_num = int(report[1])
+		self.num_blocks_in_group = int(report[2])
+		self.num_inodes_in_group = int(report[3])
+		self.num_free_blocks = int(report[4])
+		self.num_free_inodes = int(report[5])
+		self.block_bitmap = int(report[6])
+		self.inode_bitmap = int(report[7])
+		self.inode_table = int(report[8])
 	
+"""
 class Bfree:
-	def __init__(self):
-		self.block_num = -1
+	def __init__(self, report):
+		self.block_num = report[0]
 
 class Ifree:
-	def __init__(self):
-		self.inode_num = -1
-
-class Dirent:
-	def __init__(self):
-		self.parent_inode_num = -1
-		self.logical_byte_offset = -1
-		self.inode_num = -1
-		self.entry_length = -1
-		self.name_length = -1
-		self.name = -1
+	def __init__(self, report):
+		self.inode_num = report[0]
+"""
 
 class Inode:
-	def __init__(self):
-		self.inode_num = -1
-		self.file_type = -1
-		self.mode = -1
-		self.owner = -1
-		self.group = -1
-		self.link_count = -1
-		self.inode_change_time = -1 #change time of inode
-		self.inode_modification_time = -1 #modification time of file
-		self.inode_access_time = -1
-		self.file_size = -1
-		self.num_of_blocks = -1
-		#add in essentially buffers for the 12 direct block ptrs
-		self.ind_ptr = -1
-		self.dind_ptr = -1
-		self.tind_ptr = -1
+	def __init__(self, report):
+		self.inode_num = int(report[1])
+		self.file_type = report[2]
+		self.mode = int(report[3])
+		self.owner = int(report[4])
+		self.group = int(report[5])
+		self.link_count = int(report[6])
+		self.inode_change_time = report[7] #change time of inode
+		self.inode_modification_time = report[8] #modification time of file
+		self.inode_access_time = report[9]
+		self.file_size = int(report[10])
+		self.num_of_blocks = int(report[11])
+		self.list = [int(report[12]),int(report[13]),int(report[14]),int(report[15]),int(report[16]),int(report[17]),int(report[18]),int(report[19]),int(report[20]),int(report[21]),int(report[22]),int(report[23])] #add in a buffer for the 12 direct block ptrs
+		self.ind_ptr = int(report[24])
+		self.dind_ptr = int(report[25])
+		self.tind_ptr = int(report[26])
 
-with open('samples/P3B-test_1.csv', 'r') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        print(row)
+class Dirent:
+	def __init__(self, report):
+		self.parent_inode_num = int(report[1])
+		self.logical_byte_offset = int(report[2])
+		self.inode_num = int(report[3])
+		self.entry_length = int(report[4])
+		self.name_length = int(report[5])
+		self.name = report[6]
+
+class Indirect:
+	def __init__(self, report):
+		self.inode_num = int(report[1])
+		self.indirection_level = int(report[2])
+		self.logical_block_offset = int(report[3])
+		self.block_num_of_containing_reference = int(report[4])
+		self.block_num_of_referenced = int(report[5])
+
+#define auxiliary functions
+def contains(list_of_inodes,num):
+	for inode in list_of_inodes:
+		if (inode.inode_num == num):
+			return True
+	return False
+
+#instantiate "global" data structures
+sb = -1
+gdt = -1
+free_blocks_nums = []
+free_inodes_nums = []
+directory_entries = []
+allocated_inodes = []
+
+with open('samples/P3B-test_15.csv', 'r') as f:
+	output = csv.reader(f)
+	for r in output:
+		if (r[0] == 'SUPERBLOCK'):
+			sb = Superblock(r)
+		elif (r[0] == 'GROUP'):
+			gdt = Group(r)
+		elif (r[0] == 'BFREE'):
+			free_blocks_nums.append(int(r[1]))
+		elif (r[0] == 'IFREE'):
+			free_inodes_nums.append(int(r[1]))
+		elif (r[0] == 'DIRENT'):
+			directory_entries.append(Dirent(r))
+		elif (r[0] == 'INODE'):
+			allocated_inodes.append(Inode(r))
+		elif (r[0] == 'INDIRECT'):
+			print(r)
+
+#inode allocation audits
+for i in range(11,sb.inodes_count+1): #allocated_inodes
+	onFreeList = bool(i in free_inodes_nums)
+	isAllocated = bool(contains(allocated_inodes,i))
+
+	print ("audit: checking " + str(i) + ", where onFreeList = " + str(onFreeList) + " and isAllocated = " + str(isAllocated))
+
+	if onFreeList and isAllocated:
+		print("ALLOCATED INODE " + str(i) + " ON FREELIST")
+	elif (not onFreeList) and (not isAllocated):
+		print("UNALLOCATED INODE " + str(i) + " NOT ON FREELIST")
+
+#block consistency audits
+
+#inode allocation audits
